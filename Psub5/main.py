b78bdebf7fee92c165a2529371d63b1d4fe609d2
Jang99u <alswkdrb@dgu.ac.kr>
2024-04-08 15:47:30 +0900

    .

--- Psub5/main.py
+++ Psub5/main.py
@@ -1,8 +1,10 @@
 name = input("이름을 입력하세요: ")
 num = input("학번을 입력하세요: ")
 edu = input("학과을 입력하세요: ")
+grade = input("학년을 입력하세요: ")
 
 print("<출력>")
 print(name)
 print(num)
 print(edu)
+print(grade)
